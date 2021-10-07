b832243698e81cdb40410a651c3649cff6f9f75f
Michael <maj8k@yahoo.com>
2021-10-07 01:51:16 -0400

    server grabs & sends 1 msg/player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import websockets
 import asyncio
+import json
 
 PORT = 7890
 
@@ -7,30 +8,33 @@ print('Started Server')
 print('listening on port: ' + str(PORT))
 
 conns = set()
-playerdata = []
+playerdata = {}
 
 async def echo(websocket, path):
     print("A Client just connected")
     conns.add(websocket)
-    mydata = ('pcolor','scolor',0.0,0.0, 0.0,0.0,0.0,0.0,0.0,0.0)
-    playerdata.append(mydata)
+    # mydata = ('pcolor','scolor',px,px,bullets)
     try:
         async for message in websocket:
-            marker = 0
-            for conn in conns:
-                if (conn == websocket): 
-                    playerdata[marker] = mydata
-                    break
-                marker += 1
-            for conn in conns:
-                if(conn == websocket):
-                    await conn.send(playerdata)
+            mydata = (
+                message.split(',')[0],
+                message.split(',')[1],
+                float(message.split(',')[2]),
+                float(message.split(',')[3]),
+            )
+            playerdata[str(websocket)] = mydata
+            temp = []
+            for X in playerdata:
+                if (X != str(websocket)):
+                    temp.append(playerdata[X])
+            await websocket.send(json.dumps(temp))
+
     except websockets.exceptions.ConnectionClosed as e:
         print("client disconnected: ")
         print(e)
     finally:
+        playerdata.pop(str(websocket))
         conns.remove(websocket)
-        playerdata.pop()
 
 start_server = websockets.serve(echo, "localhost", PORT)
 
