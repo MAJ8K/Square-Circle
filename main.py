e184992c96aed401ef81d827e503b5012a3a2f49
Michael <maj8k@yahoo.com>
2021-10-06 21:19:09 -0400

    testing things

--- main.py
+++ main.py
@@ -7,16 +7,18 @@ print('Started Server')
 print('listening on port: ' + str(PORT))
 
 conns = set()
+playerdata = []
 
 async def echo(websocket, path):
     print("A Client just connected")
     conns.add(websocket)
+    playerdata.append(('pcolor','scolor'))
     try:
         async for message in websocket:
             print("Ping: " + message)
             for conn in conns:
                 if(conn == websocket):
-                    await conn.send("Someone said: " + message)
+                    await conn.send(playerdata)
     except websockets.exceptions.ConnectionClosed as e:
         print("client disconnected: ")
         print(e)
